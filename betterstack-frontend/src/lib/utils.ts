5ef6790e2e3a1374af0b088486d85679d3eb35cd
Deepak-negi-11 <deepaknegi108r@gmail.com>
2026-01-30 21:36:26 +0530

    fixed

--- betterstack-frontend/src/lib/utils.ts
+++ betterstack-frontend/src/lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const BACKEND_URL = "http://139.59.74.210:3001";
+export const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
